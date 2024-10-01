11162338bf4310766d9ad4adfcd75405e781c43e
fosterm-mw <michael.foster@mavenwave.com>
2024-10-01 15:50:06 -0500

    add helper function for test

--- reader_test.go
+++ reader_test.go
@@ -1,21 +1,62 @@
 package main
 
 import (
-	"log"
+	"reflect"
 	"testing"
 )
 
 func TestCRDIngestion(t *testing.T) {
-	want := &CRD{}
+	names := &CRDSpecNames{
+		Categories: []string{
+			"crossplane",
+			"composition",
+			"app",
+		},
+		Kind:   "ApClaim",
+		Plural: "appclaims",
+	}
+	want := &CRD{
+		ApiVersion: "apiextensions.crossplane.io/v1",
+		Kind:       "CompositeResourceDefinition",
+		Metadata:   map[interface{}]interface{}{"name": "xapps.app.idp.mayo.edu"},
+		Spec: CRDSpec{
+			ClaimNames:                   *names,
+			DefaultCompositeDeletePolicy: "Background",
+			DefaultCompositionRef: struct {
+				Name string `yaml:"name"`
+			}{Name: "xapps-v1alpha1.app.idp.mayo.edu"},
+			DefaultCompositionUpdatePolicy: "Automatic",
+			Group:                          "app.idp.mayo.edu",
+			Names:                          *names,
+			Versions: []CRDVersions{
+				{
+					Name:          "v1alpha1",
+					Referenceable: true,
+					Schema: struct {
+						OpenAPIV3Schema map[interface{}]interface{} `yaml:"openAPIV3Schema"`
+					}{},
+				},
+			},
+		},
+	}
 	got := &CRD{}
 	got = got.ingestCRD()
-	log.Println(got.ApiVersion)
-	log.Println(got.Spec.ClaimNames.Kind)
-	if want != got {
-		t.Fatalf("getValueByString(testData, claimName) = %v, wanted %v", got, want)
-	}
+	testCRDEquivalence(t, got, want)
 }
 
-func TestCRDApiVersionIngestion(t *testing.T) {
+func testCRDEquivalence(t *testing.T, got, want *CRD) {
+	gotValues := reflect.ValueOf(*got)
+	wantValues := reflect.ValueOf(*want)
+	gotTypes := gotValues.Type()
+	wantTypes := wantValues.Type()
 
+	for i := 0; i < gotValues.NumField(); i++ {
+		// TODO: Handle Schema
+		if gotTypes.Field(i).Name == "Schema" {
+			return
+		}
+		if !reflect.DeepEqual(gotValues.Field(i).Interface(), wantValues.Field(i).Interface()) {
+			t.Fatalf("Parsing CRD failed, got %v for %v, wanted %v for %v", gotValues.Field(i), gotTypes.Field(i).Name, wantValues.Field(i), wantTypes.Field(i).Name)
+		}
+	}
 }
